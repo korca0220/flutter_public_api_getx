80a76bbee3e60bdcfc05b286d979ff1a025310de
ParkJuneWoo <korca02220@naver.com>
2021-08-10 14:43:42 +0900

    chart sample 추가

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_public_api_getx/app/canvas/arrow_clip_path.dart';
 import 'package:flutter_public_api_getx/app/components/covid_statistics_viwer.dart';
@@ -105,6 +106,146 @@ class App extends GetView<CovidStatisticsController> {
     );
   }
 
+  Widget _covidTrendChart() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(
+          "확진자 추이",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+        ),
+        AspectRatio(
+          aspectRatio: 1.7,
+          child: Card(
+            elevation: 0,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+            // color: const Color(0xff2c4260),
+            child: BarChart(
+              BarChartData(
+                alignment: BarChartAlignment.spaceAround,
+                maxY: 20,
+                barTouchData: BarTouchData(
+                  enabled: false,
+                  touchTooltipData: BarTouchTooltipData(
+                    tooltipBgColor: Colors.transparent,
+                    tooltipPadding: const EdgeInsets.all(0),
+                    tooltipMargin: 8,
+                    getTooltipItem: (
+                      BarChartGroupData group,
+                      int groupIndex,
+                      BarChartRodData rod,
+                      int rodIndex,
+                    ) {
+                      return BarTooltipItem(
+                        rod.y.round().toString(),
+                        TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                titlesData: FlTitlesData(
+                  show: true,
+                  bottomTitles: SideTitles(
+                    showTitles: true,
+                    getTextStyles: (value) => const TextStyle(
+                        color: Color(0xff7589a2),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14),
+                    margin: 20,
+                    getTitles: (double value) {
+                      switch (value.toInt()) {
+                        case 0:
+                          return 'Mn';
+                        case 1:
+                          return 'Te';
+                        case 2:
+                          return 'Wd';
+                        case 3:
+                          return 'Tu';
+                        case 4:
+                          return 'Fr';
+                        case 5:
+                          return 'St';
+                        case 6:
+                          return 'Sn';
+                        default:
+                          return '';
+                      }
+                    },
+                  ),
+                  leftTitles: SideTitles(showTitles: false),
+                ),
+                borderData: FlBorderData(
+                  show: false,
+                ),
+                barGroups: [
+                  BarChartGroupData(
+                    x: 0,
+                    barRods: [
+                      BarChartRodData(
+                          y: 8,
+                          colors: [Colors.lightBlueAccent, Colors.greenAccent])
+                    ],
+                    showingTooltipIndicators: [0],
+                  ),
+                  BarChartGroupData(
+                    x: 1,
+                    barRods: [
+                      BarChartRodData(
+                          y: 10,
+                          colors: [Colors.lightBlueAccent, Colors.greenAccent])
+                    ],
+                    showingTooltipIndicators: [0],
+                  ),
+                  BarChartGroupData(
+                    x: 2,
+                    barRods: [
+                      BarChartRodData(
+                          y: 14,
+                          colors: [Colors.lightBlueAccent, Colors.greenAccent])
+                    ],
+                    showingTooltipIndicators: [0],
+                  ),
+                  BarChartGroupData(
+                    x: 3,
+                    barRods: [
+                      BarChartRodData(
+                          y: 15,
+                          colors: [Colors.lightBlueAccent, Colors.greenAccent])
+                    ],
+                    showingTooltipIndicators: [0],
+                  ),
+                  BarChartGroupData(
+                    x: 3,
+                    barRods: [
+                      BarChartRodData(
+                          y: 13,
+                          colors: [Colors.lightBlueAccent, Colors.greenAccent])
+                    ],
+                    showingTooltipIndicators: [0],
+                  ),
+                  BarChartGroupData(
+                    x: 3,
+                    barRods: [
+                      BarChartRodData(
+                          y: 10,
+                          colors: [Colors.lightBlueAccent, Colors.greenAccent])
+                    ],
+                    showingTooltipIndicators: [0],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     headerTopZone = Get.mediaQuery.padding.top + AppBar().preferredSize.height;
@@ -149,8 +290,11 @@ class App extends GetView<CovidStatisticsController> {
                 child: Padding(
                   padding: const EdgeInsets.all(25),
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       _todayStatistics(),
+                      SizedBox(height: 20),
+                      _covidTrendChart()
                     ],
                   ),
                 ),
